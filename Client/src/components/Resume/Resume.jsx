9bece271495e6fdaf7d6916f614ba2a8e82c5e64
Shimal Akmal D <shimalakmal12@gmail.com>
2025-04-14 22:48:18 +0530

    Update Resume.jsx

--- Client/src/components/Resume/Resume.jsx
+++ Client/src/components/Resume/Resume.jsx
@@ -43,7 +43,7 @@ const Resume = ({ textEnter, textLeave }) => {
             </motion.h3>
             <div className="streak-container">
               <a href="https://github.com/shimal007">
-                <img align="center" src="https://github-readme-stats.vercel.app/api?username=jeyachandranj&count_private=true&show_icons=true&theme=nightowl&bg_color=0,000000,441350&title_color=c56a90&text_color=ffffff&rank_icon=github&hide=prs,issues,contribs&show=reviews,prs_merged,prs_merged_percentage" alt="GitHub Stats" />
+                <img align="center" src="https://github-readme-stats.vercel.app/api?username=Shimal007&count_private=true&show_icons=true&theme=nightowl&bg_color=0,000000,441350&title_color=c56a90&text_color=ffffff&rank_icon=github&hide=prs,issues,contribs&show=reviews,prs_merged,prs_merged_percentage" alt="GitHub Stats" />
               </a>
             </div>
           </motion.div>
